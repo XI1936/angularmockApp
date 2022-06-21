edf0e7feebf8d7ef4654a133a3d3f63d3c2c3f02
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-06-21 11:22:07 +0530

    child component css update

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
+@ViewChild('pstring',{static:false})  parentString; 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterVewInit(){
+    console.log(this.parentString);
   }
 
 }
